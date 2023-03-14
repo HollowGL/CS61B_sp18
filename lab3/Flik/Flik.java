4733349ec2f0bd0ca76c9ba62a7c24811fe93327
hollowgl_mac <2363613192@qq.com>
2023-03-14 16:52:45 +0800

    ok

--- lab3/Flik/Flik.java
+++ lab3/Flik/Flik.java
@@ -1,6 +1,13 @@
 /** An Integer tester created by Flik Enterprises. */
 public class Flik {
-    public static boolean isSameNumber(Integer a, Integer b) {
+    public static boolean isSameNumber(Integer a, Integer b) { // notice that int != Integer
         return a == b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isSameNumber(125, 125));
+        System.out.println(isSameNumber(127, 127));
+        System.out.println(isSameNumber(128, 128));
+        System.out.println(isSameNumber(129, 129));
     }
 }
