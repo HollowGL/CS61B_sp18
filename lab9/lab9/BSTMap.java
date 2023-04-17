487d2fa16db478822b1e7230c870729cb70d7393
hollowgl_mac <2363613192@qq.com>
2023-04-17 15:07:23 +0800

    ...

--- lab9/lab9/BSTMap.java
+++ lab9/lab9/BSTMap.java
@@ -1,5 +1,6 @@
 package lab9;
 
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 
@@ -44,7 +45,16 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
      *  or null if this map contains no mapping for the key.
      */
     private V getHelper(K key, Node p) {
-        throw new UnsupportedOperationException();
+        if (p == null) {
+            return null;
+        }
+        int cmp = key.compareTo(p.key);
+        if (cmp < 0) {
+            return getHelper(key, p.left);
+        } else if (cmp > 0) {
+            return getHelper(key, p.right);
+        }
+        return p.value;
     }
 
     /** Returns the value to which the specified key is mapped, or null if this
@@ -52,14 +62,25 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
      */
     @Override
     public V get(K key) {
-        throw new UnsupportedOperationException();
+        return getHelper(key, root);
     }
 
     /** Returns a BSTMap rooted in p with (KEY, VALUE) added as a key-value mapping.
       * Or if p is null, it returns a one node BSTMap containing (KEY, VALUE).
      */
     private Node putHelper(K key, V value, Node p) {
-        throw new UnsupportedOperationException();
+        if (p == null) {
+            size++;
+            return new Node(key, value);
+        }
+        int cmp = key.compareTo(p.key);
+        if (cmp < 0) {
+            p.left = putHelper(key, value, p.left);
+        } else if (cmp > 0) {
+            p.right = putHelper(key, value, p.right);
+        }
+        p.value = value;
+        return p;
     }
 
     /** Inserts the key KEY
@@ -67,22 +88,47 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
      */
     @Override
     public void put(K key, V value) {
-        throw new UnsupportedOperationException();
+        root = putHelper(key, value, root);
     }
 
     /* Returns the number of key-value mappings in this map. */
     @Override
     public int size() {
-        throw new UnsupportedOperationException();
+        return size;
     }
 
     //////////////// EVERYTHING BELOW THIS LINE IS OPTIONAL ////////////////
 
+    // In-Order Traversal
+    private Set<K> keySetHelper(Node p, Set<K> kSet) {
+        if (p == null) {
+            return kSet;
+        }
+        kSet = keySetHelper(p.left, kSet);
+        kSet.add(p.key);
+        kSet = keySetHelper(p.right, kSet);
+    }
+
     /* Returns a Set view of the keys contained in this map. */
     @Override
     public Set<K> keySet() {
-        throw new UnsupportedOperationException();
+        Set<K> kSet = new HashSet<>();
+        kSet = keySetHelper(root, kSet);
+        return kSet;
     }
+
+    private Node removeHelper(K key, Node p){
+        if (p == null) {
+            return null;
+        }
+        int cmp = key.compareTo(p.key);
+        if (cmp < 0) {
+            p.left = removeHelper(key, p.left);
+        } else if (cmp > 0) {
+            p.right = removeHelper(key, p.right);
+        }
+        return null;
+    } // ????????
 
     /** Removes KEY from the tree if present
      *  returns VALUE removed,
