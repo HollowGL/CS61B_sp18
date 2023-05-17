cf255fda506dd025d98fd91869b1018c146649f2
hollowgl_mac <2363613192@qq.com>
2023-05-17 17:52:55 +0800

    ...

--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -9,6 +9,9 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    public boolean cycleFound = false;
+    public int cycleStart = -1;  // mark the start node of the cycle
+    public int cycleMarked = 2;  // connect the start node first time when cycleMarked == 2, then secnond 1;
 
     public MazeCycles(Maze m) {
         super(m);
@@ -17,8 +20,35 @@ public class MazeCycles extends MazeExplorer {
     @Override
     public void solve() {
         // TODO: Your code here!
+        distTo[0] = 0;
+        dfs(0, 0);
     }
 
     // Helper methods go here
+    private void dfs(int v, int parent) {
+        if (marked[v]) {
+            cycleFound = true;
+            cycleStart = v;
+        }
+        if (cycleFound) {
+            return;
+        }
+        marked[v] = true;
+        distTo[v] = distTo[parent] + 1;
+        announce();
+
+        for (int w: maze.adj(v)) {
+            if (w != parent) {
+                dfs(w, v);
+                if (cycleFound && cycleMarked > 0) {
+                    edgeTo[w] = v;
+                    if (w == cycleStart) {
+                        cycleMarked--;
+                    }
+                    announce();
+                }
+            }
+        }
+    }
 }
 
