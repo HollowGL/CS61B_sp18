9a25e09fd94152ff2ef5900cead0f4c8352ebe5e
hollowgl_mac <2363613192@qq.com>
2023-06-18 22:27:32 +0800

    submit: 1

--- lab11/lab11/graphs/MazeAStarPath.java
+++ lab11/lab11/graphs/MazeAStarPath.java
@@ -1,4 +1,6 @@
 package lab11.graphs;
+import edu.princeton.cs.algs4.MinPQ;
+
 
 /**
  *  @author Josh Hug
@@ -8,6 +10,21 @@ public class MazeAStarPath extends MazeExplorer {
     private int t;
     private boolean targetFound = false;
     private Maze maze;
+    private MinPQ<SearchNode> minPQ;
+    private class SearchNode implements Comparable<SearchNode> {
+        public int v;
+        public SearchNode parent;
+        public int priority;
+        public SearchNode(int v, SearchNode p) {
+            this.v = v;
+            this.parent = p;
+            this.priority = h(v);
+        }
+        @Override
+        public int compareTo(SearchNode o) {
+            return this.priority - o.priority;
+        }
+    }
 
     public MazeAStarPath(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
@@ -16,22 +33,33 @@ public class MazeAStarPath extends MazeExplorer {
         t = maze.xyTo1D(targetX, targetY);
         distTo[s] = 0;
         edgeTo[s] = s;
+        marked[s] = true;
+        announce();
+        minPQ = new MinPQ<>(m.N() * m.N());
     }
 
     /** Estimate of the distance from v to the target. */
     private int h(int v) {
-        return -1;
-    }
-
-    /** Finds vertex estimated to be closest to target. */
-    private int findMinimumUnmarked() {
-        return -1;
-        /* You do not have to use this method. */
+        return Math.abs(maze.toX(v) - maze.toX(t)) + Math.abs(maze.toY(v) - maze.toY(t));
     }
 
     /** Performs an A star search from vertex s. */
     private void astar(int s) {
         // TODO
+        minPQ.insert(new SearchNode(s, null));
+        SearchNode cur = minPQ.delMin();
+        while (cur.v != t) {
+            for (int node : maze.adj(cur.v)) {
+                if (!marked[node]) {
+                    minPQ.insert(new SearchNode(node, cur));
+                }
+            }
+            cur = minPQ.delMin();
+            edgeTo[cur.v] = cur.parent.v;
+            distTo[cur.v] = distTo[cur.parent.v] + 1;
+            marked[cur.v] = true;
+            announce();
+        }
     }
 
     @Override
